7230d075b40dac640723821c933cee5a8b22d905
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-05-30 11:35:49 -0500

    Time: 415 ms (88.43%), Space: 0B (100.00%) - LeetHub

---
+++ 597-friend-requests-i-overall-acceptance-rate/597-friend-requests-i-overall-acceptance-rate.sql
@@ -0,0 +1,5 @@
+select ROUND (
+IFNULL (
+(select count(*) from (select distinct requester_id, accepter_id from RequestAccepted) as A) / (select count(*) from (select distinct sender_id,send_to_id from FriendRequest)as B)
+    
+    , 0),2) as accept_rate
