6809f8fede6800e325722f70546fa0660d81b90e
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-08 12:35:19 -0500

    Time: 1209 ms (45.88%), Space: 0B (100.00%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select p.product_id,product_name from Product as p join Sales as s on p.product_id=s.product_id  group by p.product_id HAVING MIN(sale_date) >= "2019-01-01" AND MAX(sale_date) <= "2019-03-31";
