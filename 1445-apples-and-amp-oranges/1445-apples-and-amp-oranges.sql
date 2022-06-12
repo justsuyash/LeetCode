dc15c246c6befec6c25d287ce8dd417dd474eac9
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-12 18:57:49 -0500

    Time: 455 ms (15.58%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -2,3 +2,5 @@ select sale_date, sum(case when fruit = 'apples' then 1 else -1 end * sold_num) 
 from Sales
 group by sale_date
 order by sale_date
+
+#Since there are just 2 data points this is a better solution, the other solution can be modified for more data points
