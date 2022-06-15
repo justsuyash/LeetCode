89c050c6ee76715bd84028691aed720d16892e22
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-15 15:00:30 -0500

    Time: 373 ms (60.52%), Space: 0B (100.00%) - LeetHub

--- 619-biggest-single-number/619-biggest-single-number.sql
+++ 619-biggest-single-number/619-biggest-single-number.sql
@@ -1,1 +1,6 @@
-select max(num) as num from (select num from mynumbers group by num having count(num)=1) as a;
+WITH sub AS(
+SELECT IF(COUNT(num)=1,num,NULL) as num
+FROM MyNumbers
+GROUP BY num)
+SELECT MAX(num) as num
+FROM sub
