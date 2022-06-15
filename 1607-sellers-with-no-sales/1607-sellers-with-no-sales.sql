8c6ce7fde417fb83f8fd6e56afdbd3d44900e5e5
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-15 14:46:07 -0500

    Time: 3607 ms (5.04%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
-
-
-select seller_name from seller as s left join orders as o on s.seller_id = o.seller_id and extract(year from sale_date)='2020' where order_id is null order by seller_name asc;
+select s.seller_name
+from seller s
+     left outer join orders o on (s.seller_id = o.seller_id
+                                 and extract(year from sale_date) = 2020)
+where o.seller_id is null
+order by seller_name
