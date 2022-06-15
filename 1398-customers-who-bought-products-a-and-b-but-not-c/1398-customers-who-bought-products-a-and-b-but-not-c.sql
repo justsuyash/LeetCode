36ecc155e392d79c0ef2f1ab1bb15b5cbe93b07e
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-15 16:18:12 -0500

    Time: 519 ms (90.91%), Space: 0B (100.00%) - LeetHub

--- 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,12 +1,9 @@
-select customer_id, customer_name
-from Customers
-where customer_id in
-(select t2.customer_id
-from(select customer_id,
-sum(case when product_name = 'A' then 1 else 0 end) as 'A',
-sum(case when product_name = 'B' then 1 else 0 end) as 'B',
-sum(case when product_name = 'C' then 1 else 0 end) as 'C'
-from Orders
-group by customer_id) t2
-where A != 0 and B != 0 and C = 0)
+select a.customer_id,c.customer_name from 
 
+(
+    
+select order_id, customer_id, sum(case when product_name='A' then 1 else 0 end) as A,
+sum(case when product_name='B' then 1 else 0 end) as B,
+sum(case when product_name='C' then 1 else 0 end) as C from Orders group by customer_id
+
+    ) as a join customers as c on a.customer_id = c.customer_id where A>0 and B>0 and C=0;
