fe213fb9bd49314fc47f2795320429c7c378b299
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-15 14:47:13 -0500

    Time: 1146 ms (18.94%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -1,6 +1,8 @@
-select s.seller_name
-from seller s
-     left outer join orders o on (s.seller_id = o.seller_id
-                                 and extract(year from sale_date) = 2020)
-where o.seller_id is null
-order by seller_name
+# Write your MySQL query statement below
+
+
+select seller_name from seller where seller_name not in
+
+(
+select seller_name from seller as s left join Orders as o on s.seller_id = o.seller_id where Year(sale_date) ='2020' group by s.seller_id 
+    ) order by seller_name asc;
