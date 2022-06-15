99c0d4153860b7ef4640a378c589aac7a6eb0db8
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-15 14:48:35 -0500

    Time: 3123 ms (5.04%), Space: 0B (100.00%) - LeetHub

--- 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
+++ 1607-sellers-with-no-sales/1607-sellers-with-no-sales.sql
@@ -1,11 +1,5 @@
-SELECT seller_name
-FROM 
-Seller AS seller
-LEFT JOIN 
-(SELECT seller_id,sale_date 
- FROM Orders 
- WHERE YEAR(sale_date) = 2020
- ) orderst         
-ON  orderst.seller_id = seller.seller_id
-WHERE orderst.sale_date IS NULL
-ORDER BY seller_name ASC
+SELECT seller_name FROM Seller
+WHERE seller_id NOT IN (
+    SELECT seller_id FROM Orders
+    WHERE YEAR(sale_date)=2020)
+ORDER BY seller_name;
