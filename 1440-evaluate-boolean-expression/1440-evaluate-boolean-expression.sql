87bd1d39b88c97a936946004c4232fe9e6841621
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-17 13:04:11 -0500

    Time: 1557 ms (20.08%), Space: 0B (100.00%) - LeetHub

--- 1440-evaluate-boolean-expression/1440-evaluate-boolean-expression.sql
+++ 1440-evaluate-boolean-expression/1440-evaluate-boolean-expression.sql
@@ -1,19 +1,9 @@
-# Write your MySQL query statement below
+with cte as (select left_operand,operator,right_operand,
+case when t2.value -t3.value >0 then '>'
+when t2.value -t3.value <0 then '<'
+else '=' end as value
+from expressions t1
+left join variables t2 on t2.name = t1.left_operand
+left join variables t3 on t3.name = t1.right_operand)
 
-with cte as 
-(
-select left_operand,operator,right_operand,
-case 
-when v.value - v1.value > 0 then '>' 
-when v.value - v1.value < 0 then '<'
-else '=' end as value1
-from Variables as v join Expressions as e 
-on v.name = e.left_operand
-join 
-variables as v1
-on v1.name = e.right_operand
-)
-
-select left_operand, operator, right_operand, 
-case when operator=value1 then 'true' else 'false' end as value from cte;
-
+select left_operand,operator,right_operand, if(value = operator,'true','false') as value from cte
