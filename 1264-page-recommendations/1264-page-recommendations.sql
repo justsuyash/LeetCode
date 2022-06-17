728178a0c042f7ca96d02f0a438a144af02d1e9a
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-06-17 15:43:40 -0500

    Time: 543 ms (55.14%), Space: 0B (100.00%) - LeetHub

--- 1264-page-recommendations/1264-page-recommendations.sql
+++ 1264-page-recommendations/1264-page-recommendations.sql
@@ -1,7 +1,10 @@
-with cte as (select user_id,(page_id)
-from Likes
-where user_id in (select user2_id from Friendship where user1_id=1) or user_id in (select user1_id from Friendship where user2_id=1))
+#identify all friends of user 1
+WITH cte AS (SELECT user2_id FROM Friendship WHERE user1_id = 1
+UNION
+SELECT user1_id FROM Friendship WHERE user2_id = 1 )
 
-select distinct(page_id) as recommended_page
-from cte
-where page_id not in (select page_id from Likes where user_id=1);
+
+SELECT distinct page_id as recommended_page FROM likes
+WHERE user_id in (select * from cte)
+and page_id not in (SELECT page_id FROM likes where user_id =1)
+
