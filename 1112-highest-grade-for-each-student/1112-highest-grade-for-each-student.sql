64c423db2710e12f417749d6f62e5c4765aeb29b
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-07-03 12:57:46 -0500

    Time: 454 ms (95.71%), Space: 0B (100.00%) - LeetHub

--- 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
+++ 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
@@ -1,13 +1,10 @@
-
-
-With temp AS
-(SELECT student_id, MAX(grade) AS grade
+WITH TABLE_1
+AS
+(
+SELECT student_id , course_id , grade
+,RANK() OVER(PARTITION BY student_id ORDER BY grade DESC, course_id ASC) AS RANKED
 FROM Enrollments
-GROUP BY 1)
-
-SELECT temp.student_id, MIN(e.course_id) AS course_id, temp.grade
-FROM temp
-LEFT JOIN Enrollments e
-ON e.student_id = temp.student_id AND e.grade = temp.grade
-GROUP BY 1
-ORDER BY 1;
+)
+SELECT student_id , course_id , grade
+FROM TABLE_1
+WHERE RANKED = 1;
