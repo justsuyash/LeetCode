9136db7ea3645deed569d9cbba7688d25ed8c7f9
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-07-03 13:11:36 -0500

    Time: 489 ms (96.98%), Space: 0B (100.00%) - LeetHub

--- 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,9 +1,9 @@
-select a.customer_id,c.customer_name from 
-
-(
-    
-select order_id, customer_id, sum(case when product_name='A' then 1 else 0 end) as A,
-sum(case when product_name='B' then 1 else 0 end) as B,
-sum(case when product_name='C' then 1 else 0 end) as C from Orders group by customer_id
-
-    ) as a join customers as c on a.customer_id = c.customer_id where A>0 and B>0 and C=0;
+select customer_id, customer_name
+from customers
+where
+customer_id in (select customer_id from orders where product_name = 'A')
+and
+customer_id in (select customer_id from orders where product_name = 'B')
+and
+customer_id not in (select customer_id from orders where product_name = 'C')
+order by customer_id
