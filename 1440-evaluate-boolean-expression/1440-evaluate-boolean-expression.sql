27f3eca2ff8a7387dffd5d721773ee422f32feef
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-07-04 11:39:34 -0500

    Time: 1788 ms (16.72%), Space: 0B (100.00%) - LeetHub

--- 1440-evaluate-boolean-expression/1440-evaluate-boolean-expression.sql
+++ 1440-evaluate-boolean-expression/1440-evaluate-boolean-expression.sql
@@ -1,9 +1,11 @@
-with cte as (select left_operand,operator,right_operand,
-case when t2.value -t3.value >0 then '>'
-when t2.value -t3.value <0 then '<'
-else '=' end as value
-from expressions t1
-left join variables t2 on t2.name = t1.left_operand
-left join variables t3 on t3.name = t1.right_operand)
-
-select left_operand,operator,right_operand, if(value = operator,'true','false') as value from cte
+with cte as 
+(
+select v.name,v1.value as val1,e.operator,e.left_operand,v.value as val2,e.right_operand from expressions as e join variables as v on e.right_operand = v.name join variables v1 on e.left_operand=v1.name
+)
+select left_operand, operator, right_operand, 
+(case 
+when val1=val2 and operator = '=' then 'true'
+when val1>val2 and operator = '>' then 'true'
+when val1<val2 and operator = '<' then 'true' else 'false'
+end) as value
+from cte;
