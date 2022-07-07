292e71ca9d88d7d499ccc4ec3fddb3a979fdf718
justsuyash <59526128+justsuyash@users.noreply.github.com>
2022-07-07 11:06:48 -0500

    Time: 2347 ms (9.22%), Space: 0B (100.00%) - LeetHub

--- 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 
-with cte as 
+with cte as
 (
-    select id,name,phone_number,substring(phone_number,1,3) as country_code from person )
-    
-select name as country from (
-select avg(duration),dense_rank() over (order by avg(duration) desc) as rnk, co.name from cte as ct join Country as co on co.country_code=ct.country_code left join calls as c on c.caller_id=ct.id or c.callee_id=ct.id group by co.name having avg(duration) > (select avg(duration) from calls)) as a
+select id, name, left(phone_number,3) as country_code from Person
+)
+select country from (
+select co.name as country, avg(c.duration) as avgdur from cte as p join country as co on p.country_code = co.country_code join calls as c on c.caller_id = p.id or c.callee_id = p.id group by country
+    ) as a where avgdur > (select avg(duration) from calls);
