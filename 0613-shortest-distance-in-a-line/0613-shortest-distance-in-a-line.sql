ed60f0c07690b21eac3af1252c7b4029ea376661
justsuyash <59526128+justsuyash@users.noreply.github.com>
2023-12-03 13:55:03 -0600

    Time: 1403 ms (5.08%), Space: 0B (100.00%) - LeetHub

--- 0613-shortest-distance-in-a-line/0613-shortest-distance-in-a-line.sql
+++ 0613-shortest-distance-in-a-line/0613-shortest-distance-in-a-line.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 
 
-select MIN(ABS(p1.x-p2.x)) as shortest from point as p1 
-inner join point as p2 on p2.x!=p1.x
+select min(p1.x - p2.x) shortest
+from point p1
+inner join point p2 on p1.x > p2.x
